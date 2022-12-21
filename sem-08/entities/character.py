b976d994eb84683ac7cac04afd731c36bea0c583
DINODEWD <63123041+DINODEWD@users.noreply.github.com>
2022-12-21 21:14:41 +0200

    Create character.py

---
+++ sem-08/entities/character.py
@@ -0,0 +1,9 @@
+class Character:
+    def __init__(self, name, sex, ch_class, weapon, extra_obj):
+        self.name = name
+        self.sex = sex
+        self.ch_class = ch_class
+        self.weapon = weapon
+        self.extra_obj = extra_obj
+
+    
